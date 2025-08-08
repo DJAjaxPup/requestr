4ce553d5e0d8eb25af82f35ee8dfeaa9cb243635
DJAjaxPup <shawn.a.brier@gmail.com>
2025-08-08 16:10:46 -0700

    Update socket.js

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,2 +1,11 @@
+// client/src/lib/socket.js
 import { io } from 'socket.io-client';
-export const socket = io('/', { transports: ['websocket'] });
+
+export const socket = io('/', {
+  transports: ['websocket'],   // skip long-polling for speed/stability on Render
+  autoConnect: true,
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 500,
+  reconnectionDelayMax: 3000,
+});
